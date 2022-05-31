ce47dcde0cf034f16d58b3b63b54ad5f771921fd
Calin Neamtu <nilac8991@gmail.com>
2022-05-31 18:47:17 +0200

    Update package name & switch GBoard's profile params with EKB one
    Signed-off-by: Calin Neamtu <nilac8991@gmail.com>

--- app/src/main/java/com/zebra/jamesswinton/togglenullkeyboard/App.java
+++ app/src/main/java/com/zebra/jamesswinton/togglenullkeyboard/App.java
@@ -5,26 +5,28 @@ import android.app.Application;
 public class App extends Application {
 
     // Constants
-    public static final String DEFAULT_KEYBOARD_PROFILE_NAME = "GBoard";
+    public static final String DEFAULT_KEYBOARD_PROFILE_NAME = "EnterpriseKeyboard";
     public static final String DEFAULT_KEYBOARD_PROFILE_XML =
-                    "<wap-provisioningdoc>\n" +
+            "<wap-provisioningdoc>\n" +
                     "  <characteristic type=\"Profile\">\n" +
-                    "    <parm name=\"ProfileName\" value=\"GBoard\"/>\n" +
-                    "    <characteristic version=\"10.1\" type=\"UiMgr\">\n"
-                  + "      <parm name=\"InputMethodAction\" value=\"1\" />\n"
-                  + "      <characteristic type=\"InputMethodDetails\">\n"
-                  + "        <parm name=\"InputMethodOption\" value=\"1\" />\n"
-                  + "      </characteristic>\n"
-                  + "    </characteristic>\n" +
+                    "    <parm name=\"ProfileName\" value=\"EnterpriseKeyboard\"/>\n" +
+                    "    <characteristic version=\"4.3\" type=\"UiMgr\">\n"
+                    + "      <parm name=\"InputMethodAction\" value=\"1\" />\n"
+                    + "      <characteristic type=\"InputMethodDetails\">\n"
+                    + "        <parm name=\"InputMethodOption\" value=\"4\" />\n"
+                    + "        <parm name=\"InputMethodPackageName\" value=\"com.zebra.mdna.enterprisekeyboard\" />\n"
+                    + "        <parm name=\"InputMethodClassName\" value=\"com.android.inputmethod.latin.LatinIME\" />\n"
+                    + "      </characteristic>\n"
+                    + "    </characteristic>\n" +
                     "  </characteristic>" +
                     "</wap-provisioningdoc>";
 
     public static final String NULL_KEYBOARD_PROFILE_NAME = "NullKeyboard";
     public static final String NULL_KEYBOARD_PROFILE_XML =
-                      "<wap-provisioningdoc>\n" +
-                      "  <characteristic type=\"Profile\">\n" +
-                      "    <parm name=\"ProfileName\" value=\"NullKeyboard\"/>\n" +
-                      "    <characteristic version=\"4.3\" type=\"UiMgr\">\n"
+            "<wap-provisioningdoc>\n" +
+                    "  <characteristic type=\"Profile\">\n" +
+                    "    <parm name=\"ProfileName\" value=\"NullKeyboard\"/>\n" +
+                    "    <characteristic version=\"4.3\" type=\"UiMgr\">\n"
                     + "      <parm name=\"ClipBoardClear\" value=\"false\" />\n"
                     + "      <parm name=\"InputMethodAction\" value=\"1\" />\n"
                     + "      <characteristic type=\"InputMethodDetails\">\n"
@@ -33,7 +35,7 @@ public class App extends Application {
                     + "        <parm name=\"InputMethodClassName\" value=\"com.wparam.nullkeyboard.NullKeyboard\" />\n"
                     + "      </characteristic>\n"
                     + "    </characteristic>\n" +
-                      "  </characteristic>" +
-                      "</wap-provisioningdoc>";
+                    "  </characteristic>" +
+                    "</wap-provisioningdoc>";
 
 }
